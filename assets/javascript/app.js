7a63872ed4944acfbfb9e47349c4df0cfb10c96c
Sam Lasky <Laskysm@gmail.com>
2018-03-05 18:15:31 -0500

    timer save

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,65 @@
-var form = document.querySelector("form");
-var log = document.querySelector("#log");
 
-form.addEventListener("submit", function(event) {
-  var data = new FormData(form);
-  var output = "";
-  for (const entry of data) {
-    output = entry[0] + "=" + entry[1] + "\r";
-  };
-  log.innerText = output;
-  event.preventDefault();
-}, false);
+window.onload = function() {
+
+  // maybe have to put everything in this?
+
+var intervalId;
+
+// He had it set to false
+var clockRunning = true;
+
+var stopwatch = {
+
+  time: 60,
+
+count: function() {
+
+  stopwatch.time--;
+
+  var converted = stopwatch.timeCoverter(stopwatch.time);
+  console.log(converted);
+
+  $("#display").text(converted);
+},
+
+
+timeConverter: function(t) {
+
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+
+}
+
+};
+
+
+
+function handleClick()
+  {         
+var amountCorrect = 0;          
+for(var i = 1; i <= 45; i++) {
+  var radios = document.getElementsByName('group'+i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if(radio.value == "correct" && radio.checked) {
+      amountCorrect++;
+    }
+  }
+ }                   
+    alert("Correct Responses: " + amountCorrect);
+  }
