169d8cc8768f78d216adcd806c863ee44f62af7c
Sam Lasky <Laskysm@gmail.com>
2018-03-06 12:37:49 -0500

    Fixed endGame Function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,57 @@
-window.onload = function() {
-  var stopwatch = {
-    time: 60,
-    clockRunning: true,
+var stopwatch = {
+  time: 60,
+  clockRunning: true,
 
-    count: function() {
-      stopwatch.time--;
+  count: function() {
+    stopwatch.time--;
 
-      // if time == 0 ...
-      if (stopwatch.time === 0) {
-        clearInterval(stopwatch.intervalId);
+    // if time == 0 ...
+    if (stopwatch.time === 0) {
+      clearInterval(stopwatch.intervalId);
+      endGame();
+    }
+
+    var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+
+    $("#display").text(converted);
+  },
+
+  timeConverter: function(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+}
+stopwatch.intervalId = setInterval(stopwatch.count, 1000);
+ 
+function handleClick() {
+  endGame();
+}
+
+// Need to put alert outside of if statement
+function endGame() {
+  var amountCorrect = 0;          
+  for(var i = 1; i <= 45; i++) {
+    var radios = document.getElementsByName('group'+i);
+    for(var j = 0; j < radios.length; j++) {
+      var radio = radios[j];
+      if(radio.value == "correct" && radio.checked) {
+        amountCorrect++;
       }
-
-      var converted = stopwatch.timeConverter(stopwatch.time);
-      console.log(converted);
-
-      $("#display").text(converted);
-    },
-
-    timeConverter: function(t) {
-      var minutes = Math.floor(t / 60);
-      var seconds = t - (minutes * 60);
-
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-
-      if (minutes === 0) {
-        minutes = "00";
-      }
-      else if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-
-      return minutes + ":" + seconds;
     }
   }
-
-  stopwatch.intervalId = setInterval(stopwatch.count, 1000);
-
-  function handleClick() {         
-    var amountCorrect = 0;          
-    for(var i = 1; i <= 45; i++) {
-      var radios = document.getElementsByName('group'+i);
-      for(var j = 0; j < radios.length; j++) {
-        var radio = radios[j];
-        if(radio.value == "correct" && radio.checked) {
-          amountCorrect++;
-        }
-      }
-    }                   
-    alert("Correct Responses: " + amountCorrect);
-  }
+  alert("Correct Response: " + amountCorrect);
 };
