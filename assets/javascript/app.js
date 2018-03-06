5389568ce8212c90621cbb9893e76674807c9cfb
Sam Lasky <Laskysm@gmail.com>
2018-03-06 10:22:11 -0500

    Timer Fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,54 @@
-
-
-
 window.onload = function() {
+  var stopwatch = {
+    time: 60,
+    clockRunning: true,
 
-  // maybe have to put everything in this?
+    count: function() {
+      stopwatch.time--;
 
-var intervalId;
+      // if time == 0 ...
+      if (stopwatch.time === 0) {
+        clearInterval(stopwatch.intervalId);
+      }
 
-// He had it set to false
-var clockRunning = false;
+      var converted = stopwatch.timeConverter(stopwatch.time);
+      console.log(converted);
 
-var stopwatch = {
+      $("#display").text(converted);
+    },
 
-  time: 60,
+    timeConverter: function(t) {
+      var minutes = Math.floor(t / 60);
+      var seconds = t - (minutes * 60);
 
-intervalId = setInterval(stopwatch.count, 1000),
-clockRunning = true,
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
 
-count: function() {
+      if (minutes === 0) {
+        minutes = "00";
+      }
+      else if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
 
-  stopwatch.time--;
-
-  var converted = stopwatch.timeCoverter(stopwatch.time);
-  console.log(converted);
-
-  $("#display").text(converted);
-},
-
-
-timeConverter: function(t) {
-
-  var minutes = Math.floor(t / 60);
-  var seconds = t - (minutes * 60);
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  if (minutes === 0) {
-    minutes = "00";
-  }
-  else if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  return minutes + ":" + seconds;
-}
-
-
-}
-
-};
-
-
-
-function handleClick()
-  {         
-var amountCorrect = 0;          
-for(var i = 1; i <= 45; i++) {
-  var radios = document.getElementsByName('group'+i);
-  for(var j = 0; j < radios.length; j++) {
-    var radio = radios[j];
-    if(radio.value == "correct" && radio.checked) {
-      amountCorrect++;
+      return minutes + ":" + seconds;
     }
   }
- }                   
+
+  stopwatch.intervalId = setInterval(stopwatch.count, 1000);
+
+  function handleClick() {         
+    var amountCorrect = 0;          
+    for(var i = 1; i <= 45; i++) {
+      var radios = document.getElementsByName('group'+i);
+      for(var j = 0; j < radios.length; j++) {
+        var radio = radios[j];
+        if(radio.value == "correct" && radio.checked) {
+          amountCorrect++;
+        }
+      }
+    }                   
     alert("Correct Responses: " + amountCorrect);
   }
+};
